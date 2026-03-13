c83521cc5acb93501ade29511a96c71309bd7945
Camila Silva <camila.a.silva19@aluno.senai.br>
2026-03-13 14:56:11 -0300

    sinto muito, mas não consegui

--- comparar-escolas.js
+++ comparar-escolas.js
@@ -21,20 +21,40 @@ class Senai {
 }
 /* 🚨NÃO ALTERAR A CLASSE 🚨 */
 
-
+function msg(mensagem) {
+  textoResultado.textContent = mensagem;
+};
 
 /* ===== OBTER ELEMENTOS ===== */
 
-// Captura dos elementos da unidade A e B
-const cidade = document.querySelector('unidade__input unidadeA__cidade');
-const codigo = document.querySelector('unidade__input unidadeA__codigo');
-const anoConstrucao = document.querySelector('unidade__input unidadeA__ano');
-const qtdeCursos = document.querySelector('unidade__input unidadeA__cursos');
-const formulario = document.querySelector('sistema__unidades')
+// Captura dos elementos da unidade A
+const cidadeA = document.querySelector('.unidadeA__cidade');
+const codigoA = document.querySelector('.unidadeA__codigo');
+const anoA= document.querySelector('.unidadeA__ano');
+const cursosA = document.querySelector('.unidadeA__cursos');
+const btnA = document.querySelector('.unidadeA__instanciar');
+const abrirA = document.querySelector('.unidadeA__abrir');
+const fecharA = document.querySelector('.unidadeA__fechar');
 
 
+// Captura dos elementos da unidade B
+const cidadeB = document.querySelector('.unidadeB__cidade');
+const codigoB = document.querySelector('.unidadeB__codigo');
+const anoB = document.querySelector('.unidadeB__ano');
+const cursosB = document.querySelector('.unidadeB__cursos');
+const btnB = document.querySelector('.unidadeB__instanciar');
+const abrirB = document.querySelector('.unidadeB__abrir');
+const fecharB = document.querySelector('.unidadeB__fechar');
 
 /* ===== FUNÇÃO DE VALIDAÇÃO ===== */
+
+function validacao(cidadeA, codigoA, anoA, cursosA){
+  if (cidadeA.value, codigoA.value, anoA.value, cursosA.value){
+    return 'Criada'
+  } else {
+    msg('Preencha todos os campos para criar a escola.')
+  }
+}
 
 
 /* ===== INSTANCIAR ESCOLA A ===== */
@@ -50,6 +70,24 @@ const formulario = document.querySelector('sistema__unidades')
 
 
 /* ===== RELATÓRIO DE COMPARAÇÃO ===== */
+
+const compararUnidade = (uniA, uniB) => {
+  msg(`> Veredito: ${uniA.cidade} lidera com ${uniA.qtdeCursos} cursos...`)
+
+  if (uniA.qtdeCursos > uniB.qtdeCursos) {
+      msg(`Unidade A
+          Código ${uniA.codigo} - Cidade ${uniA.cidade}
+          Fundação ${uniA.anoConstrucao} (${uniA.idade})
+          Oferta ${uniA.qtdeCursos}`);
+  } else if (uniB.qtdeCursos > uniA.qtdeCursos) {
+      msg(`Unidade B
+          Código ${uniB.codigo} - Cidade ${uniB.cidade}
+          Fundação ${uniB.anoConstrucao} (${uniB.idade})
+          Oferta ${uniB.qtdeCursos}`);
+  } else {
+      msg(`Resultado: Ambas as unidades possuem a mesma quantidade de cursos.`);
+  }
+}
 
 
 /* ===== NOVA CONSULTA ===== */
