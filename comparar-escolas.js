e53c4aeb4659cc45e639c11d2e658c04594ecaab
Camila Silva <camila.a.silva19@aluno.senai.br>
2026-03-13 08:37:42 -0300

    criando o elemento de captura

---
+++ comparar-escolas.js
@@ -0,0 +1,104 @@
+// Captura dos elementos da unidade A e B
+const cidade = document.querySelector('unidade__input unidadeA__cidade', 'unidade__input unidadeB__cidade');
+const codigo = document.querySelector('unidade__input unidadeA__codigo','unidade__input unidadeB__codigo');
+const anoConstrucao = document.querySelector('unidade__input unidadeA__ano', 'unidade__input unidadeB__ano');
+const qtdeCursos = document.querySelector('unidade__input unidadeA__cursos', 'unidade__input unidadeB__cursos');
+
+
+/* 🚨NÃO ALTERAR A CLASSE 🚨 */
+
+class Senai {
+  constructor(codigo, cidade, anoConstrucao, qtdeCursos) {
+    this.codigo = codigo;
+    this.cidade = cidade;
+    this.anoConstrucao = anoConstrucao;
+    this.qtdeCursos = qtdeCursos;
+
+    this.idade = new Date().getFullYear() - anoConstrucao;
+    this.statusFuncionamento = false;
+  }
+
+  abrirEscola() {
+    this.statusFuncionamento = true;
+    mostrarMensagem(`Aberta: Bem-vindos ao SENAI ${this.cidade}`)
+  }
+
+  fecharEscola() {
+    this.statusFuncionamento = false;
+    mostrarMensagem('Fechada: Atividades encerradas.')
+  }
+}
+/* 🚨NÃO ALTERAR A CLASSE 🚨 */
+
+function mostrarMensagem(mensagem) {
+  textoResultado.textContent = mensagem;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* SUGESTÃO DE LÓGICA PARA O DESENVOLVIMENTO */
+
+
+
+/* ===== OBTER ELEMENTOS ===== */
+
+
+/* ===== FUNÇÃO DE VALIDAÇÃO ===== */
+
+
+/* ===== INSTANCIAR ESCOLA A ===== */
+
+
+/* ===== INSTANCIAR ESCOLA B ===== */
+
+
+/* ===== ABRIR ESCOLA ===== */
+
+
+/* ===== FECHAR ESCOLA ===== */
+
+
+/* ===== RELATÓRIO DE COMPARAÇÃO ===== */
+
+
+/* ===== NOVA CONSULTA ===== */
+
+
