098f31b0c17a3f2b266c5c19ac6868efa679f607
Camila Silva <camila.a.silva19@aluno.senai.br>
2026-03-13 08:58:53 -0300

    modificação

--- comparar-escolas.js
+++ comparar-escolas.js
@@ -22,13 +22,14 @@ class Senai {
 /* 🚨NÃO ALTERAR A CLASSE 🚨 */
 
 
+
 /* ===== OBTER ELEMENTOS ===== */
 
 // Captura dos elementos da unidade A e B
-const cidade = document.querySelector('unidade__input unidadeA__cidade', 'unidade__input unidadeB__cidade');
-const codigo = document.querySelector('unidade__input unidadeA__codigo','unidade__input unidadeB__codigo');
-const anoConstrucao = document.querySelector('unidade__input unidadeA__ano', 'unidade__input unidadeB__ano');
-const qtdeCursos = document.querySelector('unidade__input unidadeA__cursos', 'unidade__input unidadeB__cursos');
+const cidade = document.querySelector('unidade__input unidadeA__cidade');
+const codigo = document.querySelector('unidade__input unidadeA__codigo');
+const anoConstrucao = document.querySelector('unidade__input unidadeA__ano');
+const qtdeCursos = document.querySelector('unidade__input unidadeA__cursos');
 const formulario = document.querySelector('sistema__unidades')
 
 
